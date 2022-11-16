e3bae7a556c691bd0e4b471704b410b33ac086fd
Snehasis <snehasisghosh2017@gmail.com>
2022-11-16 22:06:36 +0530

    edited local host

--- y/firebase/readdata.js
+++ y/firebase/readdata.js
@@ -1,177 +1,139 @@
-
-import {app} from './firebase.js'
-import { getFirestore, updateDoc, doc, getDoc,  } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js';
+import { app } from "./firebase.js";
+import {
+  getFirestore,
+  updateDoc,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js";
 
 // Initialize Cloud Firestore and get a reference to the service
-console.log()
+console.log();
 const db = getFirestore(app);
 
-let userdata=localStorage.getItem('user-email')
-let name
+let userdata = localStorage.getItem("user-email");
+let name;
 const docRef = doc(db, "users", userdata);
 const docSnap = await getDoc(docRef);
 
-
-// let updateData=async()=>{
-//   let data=document.querySelector('#mc').textContent
-
-//   await updateDoc(docRef, {
-//     cart:['gay']
-//   });
-//   console.log('done')
-// }
-
-
-let items=[]
+let items = [];
 let addCart = () => {
-    let carts = document.querySelectorAll('.add-cart')
-    console.log(carts)
+  let carts = document.querySelectorAll(".add-cart");
+  console.log(carts);
 
+  for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener("click", () => {
+      let pdName = document.querySelectorAll(".product-name");
+      let pdPrice = document.querySelectorAll(".price");
 
- for (let i = 0; i < carts.length; i++) {
-        carts[i].addEventListener('click', () => {
-        
-            let pdName = document.querySelectorAll('.product-name')
-            let pdPrice = document.querySelectorAll('.price')
-           
-               
-            let product = {
-                name: pdName[i].innerText,
-                price: pdPrice[i].innerText,
-                
-            }
-            items.push(product)
+      let product = {
+        name: pdName[i].innerText,
+        price: pdPrice[i].innerText,
+      };
+      items.push(product);
 
-updateCart(items)
+      updateCart(items);
 
-            carts[i].innerText="Already Added"
-            carts[i].disabled = true;
-            
-            console.log(items)
-         
-            localStorage.setItem('cart',JSON.stringify(items))
-            localStorage.setItem('count',items.length)
-            let count= localStorage.getItem('count')
-            console.log(count)
-            if(count==null){
-                count=0
-            }
-            document.querySelector(".total").innerText=count
-        }
-        )
-    }
-}
+      carts[i].innerText = "Already Added";
+      carts[i].disabled = true;
 
+      console.log(items);
 
-
+      localStorage.setItem("cart", JSON.stringify(items));
+      localStorage.setItem("count", items.length);
+      let count = localStorage.getItem("count");
+      console.log(count);
+      if (count == null) {
+        count = 0;
+      }
+      document.querySelector(".total").innerText = count;
+    });
+  }
+};
 
 if (docSnap.exists()) {
-    name=docSnap.data()
+  name = docSnap.data();
   console.log("Document data:", name);
-  try{
-    
-  document.querySelector('#user_name').textContent=`Welcome ${name.name} 👋`
-  }catch(e){}
-  try{
- document.querySelector('#my_cart').textContent=`${name.name}'s Shopping Cart 😎`    
-    }catch(e){}
-  
-  
-  items=name.cart
-  localStorage.setItem('cart',JSON.stringify(items))
-  localStorage.setItem('count',items.length)
-// document.getElementById('ok').addEventListener('click',async()=>{
-//  await updateData()
-// })
-let count= localStorage.getItem('count')
-if(count==null){
-  count=0
-}
-document.querySelector(".total").innerText=count
+  try {
+    document.querySelector(
+      "#user_name"
+    ).textContent = `Welcome ${name.name} 👋`;
+  } catch (e) {}
+  try {
+    document.querySelector(
+      "#my_cart"
+    ).textContent = `${name.name}'s Shopping Cart 😎`;
+  } catch (e) {}
+
+  items = name.cart;
+  localStorage.setItem("cart", JSON.stringify(items));
+  localStorage.setItem("count", items.length);
+  // document.getElementById('ok').addEventListener('click',async()=>{
+  //  await updateData()
+  // })
+  let count = localStorage.getItem("count");
+  if (count == null) {
+    count = 0;
+  }
+  document.querySelector(".total").innerText = count;
 } else {
   // doc.data() will be undefined in this case
   console.log("No such document!");
 }
 
+addCart();
 
-addCart()
-
- let updateCart=async(data)=>{
- 
-
+let updateCart = async (data) => {
   await updateDoc(docRef, {
-    cart:data
+    cart: data,
   });
-  console.log('done')
-}
+  console.log("done");
+};
 
-console.log(window.location.pathname)
+console.log(window.location.pathname);
 
-if(window.location.pathname=='/cart.html'){
+if (
+  window.location.pathname == "/cart.html" ||
+  window.location.pathname == "/y/cart.html"
+) {
+  let count = localStorage.getItem("count");
+  // console.log(count)
+  if (count == null) {
+    count = 0;
+  }
+  document.querySelector(".total").innerText = count;
 
-
-    let count = localStorage.getItem('count')
-    // console.log(count)
-    if (count == null) {
-        count = 0
-    }
-    document.querySelector(".total").innerText = count
-  
-
-
-
-
-
-
-    let lol = document.querySelector('#contents')
-    let ans = document.querySelector('#ans')
-    let items = localStorage.getItem('cart')
-    items = JSON.parse(items)
-    //  console.log(items)
-    items.map(e => {
-        console.log(e)
-        lol.innerHTML += `
+  let lol = document.querySelector("#contents");
+  let ans = document.querySelector("#ans");
+  let items = localStorage.getItem("cart");
+  items = JSON.parse(items);
+  //  console.log(items)
+  items.map((e) => {
+    console.log(e);
+    lol.innerHTML += `
     <tr>
     <td>${e.name}</td>
     <td>Rs : ${e.price}</td>
     <td ><button class="delete-item">Remove</button></td>
 </tr>`;
+  });
+  let totalCost = 0;
+  for (let t of items) {
+    totalCost += parseFloat(t.price.replace(/,/g, ""));
+  }
+  console.log(totalCost);
+  ans.innerText = totalCost;
 
+  let allitems = document.querySelectorAll(".delete-item");
 
-    })
-    let totalCost = 0;
-    for (let t of items) {
-        totalCost += parseFloat(t.price.replace(/,/g, ''))
-    }
-    console.log(totalCost)
-    ans.innerText = totalCost
+  for (let i = 0; i < allitems.length; i++) {
+    allitems[i].addEventListener("click", async () => {
+      console.log("deleete");
+      items.splice(i, 1);
+      await updateCart(items);
 
-
-
-
-
-
-
-   
-    let allitems = document.querySelectorAll('.delete-item')
-
-    for (let i = 0; i < allitems.length; i++) {
-        allitems[i].addEventListener('click', async() => {
-            console.log('deleete')
-            items.splice(i, 1);
-          await  updateCart(items)
-
-
-            localStorage.setItem('cart', JSON.stringify(items))
-            localStorage.setItem('count', JSON.stringify(items.length))
-            location.reload()
-        }
-        )
-
-
-    }
-
-
-
-
+      localStorage.setItem("cart", JSON.stringify(items));
+      localStorage.setItem("count", JSON.stringify(items.length));
+      location.reload();
+    });
+  }
 }
